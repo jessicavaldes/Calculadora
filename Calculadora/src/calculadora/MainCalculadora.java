4f906962ee3c0792a0a6e358ab7e129ed4c65a38
jessicavaldes <jessicavaldes.flores@gmail.com>
2016-10-06 08:42:58 -0500

    Add files via upload

---
+++ Calculadora/src/calculadora/MainCalculadora.java
@@ -0,0 +1,13 @@
+package calculadora;
+
+/**
+ *@author:  jessvaldes 
+ * @version:  1.0 
+ * @date: 27-09-2016 
+ */
+public class MainCalculadora {
+ public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		calculadora.setVisible(true);
+	}   
+}
